d6f4d298f365dbfcae417240cc038ed88ff9e414
payne-chris-r <06cpayne@gmail.com>
2017-03-31 23:35:04 -0400

    Updates from browser-template
    Updates from browser-template, moving some config stuff around,
    but mostly just converting to standard-js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,21 +1,21 @@
-'use strict';
+'use strict'
 
-const setAPIOrigin = require('../../lib/set-api-origin');
-const config = require('./config');
+const setAPIOrigin = require('../../lib/set-api-origin')
+const config = require('./config')
 
 $(() => {
-  setAPIOrigin(location, config);
-});
+  setAPIOrigin(location, config)
+})
 
 // use require with a reference to bundle the file and use it in this file
-// const example = require('./example');
+// const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
-// require('./example');
+require('./example')
 
-const authEvents = require('./auth/events.js');
+const authEvents = require('./auth/events.js')
 
 // On document ready
 $(() => {
-  authEvents.addHandlers();
-});
+  authEvents.addHandlers()
+})
